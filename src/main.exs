77af7796c6383bcd049dee4018ae4d0f6da03fbd
nicozu <nzulaica@fi.uba.ar>
2023-12-19 16:53:40 -0300

    stream file data

--- src/main.exs
+++ src/main.exs
@@ -1,16 +1,18 @@
 defmodule Messages do
-  def sender(receiver, messages) do
-    spawn_link(fn -> send_messages(receiver, messages) end)
+  def sender(receiver, file_name) do
+    spawn_link(fn -> send_messages(receiver, file_name) end)
   end
 
-  defp send_messages(receiver, messages) do
-    Enum.each(messages, &send_message(receiver, &1))
+  defp send_messages(receiver, file_name) do
+    file_name
+    |> File.stream!()
+    |> Enum.each(&send_message(receiver, &1))
+
     send(receiver, {self(), :stop})
   end
 
   defp send_message(receiver, message) do
-    send(receiver, {self(), message})
-    :timer.sleep(1000) # wait for 1 second
+    send(receiver, {self(), String.trim(message)})
   end
 
   def receiver do
@@ -27,7 +29,15 @@ defmodule Messages do
         receive_messages()
     end
   end
+
+  def wait_for_process(pid) do
+    Process.monitor(pid)
+    receive do
+      {:DOWN, _ref, :process, ^pid, _reason} -> :ok
+    end
+  end
 end
 
 receiver = Messages.receiver()
-Messages.sender(receiver, ["Hello", "World"])
+Messages.sender(receiver, "data.txt")
+Messages.wait_for_process(receiver)
