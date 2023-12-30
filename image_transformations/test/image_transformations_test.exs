db454fe7e0d57726fc1f625f337984e99d989fa7
n-zu <core.editor>
2023-12-29 22:54:09 -0300

    setup business logic

---
+++ image_transformations/test/image_transformations_test.exs
@@ -0,0 +1,8 @@
+defmodule ImageTransformationsTest do
+  use ExUnit.Case
+  doctest ImageTransformations
+
+  test "greets the world" do
+    assert ImageTransformations.hello() == :world
+  end
+end
