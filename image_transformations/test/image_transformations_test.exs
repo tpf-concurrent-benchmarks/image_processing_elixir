48c2c17bb18241493039be246c824f833c24b47e
n-zu <core.editor>
2024-01-04 20:12:28 -0300

    cleanup
    remove old business logic playground

--- image_transformations/test/image_transformations_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ImageTransformationsTest do
-  use ExUnit.Case
-  doctest ImageTransformations
-
-  test "greets the world" do
-    assert ImageTransformations.hello() == :world
-  end
-end
